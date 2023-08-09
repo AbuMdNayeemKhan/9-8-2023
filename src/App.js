760c9dd11813414d0d9cbdaa159bb12a19540b40
unknown <abumohammadnayemkhan@gmail.com>
2023-08-09 22:52:30 +0600

    React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './pages/Home';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <div>
+        <h1>Nayeeem</h1>
+        <h1>Zioun</h1>
+        <h1>Tishat</h1>
+        <h1>Rohan</h1>
+        <h1>Rafi</h1>
+        <h1>Sohan</h1>
+    </div>
+  )
+}
